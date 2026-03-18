4ce587ab9da5e869aaf3af74b9f54856e04d2dfd
Oskar Jonsson <oskarjonsson.dev@gmail.com>
2026-03-18 21:43:49 +0100

    Add VoteService with createVote function

---
+++ tripsync/src/main/java/com/tripsync/tripsync/service/VoteService.java
@@ -0,0 +1,19 @@
+package com.tripsync.tripsync.service;
+
+import com.tripsync.tripsync.model.Vote;
+import com.tripsync.tripsync.repository.VoteRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VoteService {
+
+    private final VoteRepository voteRepository;
+
+    public VoteService(VoteRepository voteRepository) {
+        this.voteRepository = voteRepository;
+    }
+
+    public Vote createVote(Vote vote) {
+        return voteRepository.save(vote);
+    }
+}
