e25c5200cec9fc0450d22ff313631c123dc4abe9
Oskar Jonsson <oskarjonsson.dev@gmail.com>
2026-03-18 22:14:52 +0100

    Add findByTripId to PlanRepository

--- tripsync/src/main/java/com/tripsync/tripsync/repository/PlanRepository.java
+++ tripsync/src/main/java/com/tripsync/tripsync/repository/PlanRepository.java
@@ -3,5 +3,9 @@ package com.tripsync.tripsync.repository;
 import com.tripsync.tripsync.model.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PlanRepository extends JpaRepository<Plan, Long> {
+
+    List<Plan> findByTripId(Long tripId);
 }
