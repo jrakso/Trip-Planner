106162c87b7b82254db5584bc6fcdc8fa488f6ee
Oskar Jonsson <oskarjonsson.dev@gmail.com>
2026-03-18 21:27:08 +0100

    Add PlanService with createPlan function

---
+++ tripsync/src/main/java/com/tripsync/tripsync/service/PlanService.java
@@ -0,0 +1,20 @@
+package com.tripsync.tripsync.service;
+
+import com.tripsync.tripsync.model.Plan;
+import com.tripsync.tripsync.repository.PlanRepository;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlanService {
+
+    private final PlanRepository planRepository;
+
+    public PlanService(PlanRepository planRepository) {
+        this.planRepository = planRepository;
+    }
+
+    public Plan createPlan(Plan plan) {
+        return planRepository.save(plan);
+    }
+}
