1eb47aff46533fd3c309cc0636e7da4413c31e95
Oskar Jonsson <oskarjonsson.dev@gmail.com>
2026-03-18 23:30:54 +0100

    Add PlanSummary Data Transfer Object

---
+++ tripsync/src/main/java/com/tripsync/tripsync/dto/PlanSummaryDTO.java
@@ -0,0 +1,32 @@
+package com.tripsync.tripsync.dto;
+
+public class PlanSummaryDTO {
+
+    private Long planId;
+    private String title;
+    private int yesVotes;
+    private int noVotes;
+
+    public PlanSummaryDTO(Long planId, String title, int yesVotes, int noVotes) {
+        this.planId = planId;
+        this.title = title;
+        this.yesVotes = yesVotes;
+        this.noVotes = noVotes;
+    }
+
+    public Long getPlanId() {
+        return planId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYesVotes() {
+        return yesVotes;
+    }
+
+    public int getNoVotes() {
+        return noVotes;
+    }
+}
