73d8042dbd1f1b2ab65748b6aae7f11159e88728
Oskar Jonsson <oskarjonsson.dev@gmail.com>
2026-03-18 21:02:36 +0100

    Add TripRepository

---
+++ tripsync/src/main/java/com/tripsync/tripsync/repository/TripRepository.java
@@ -0,0 +1,7 @@
+package com.tripsync.tripsync.repository;
+
+import com.tripsync.tripsync.model.Trip;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TripRepository extends JpaRepository<Trip, Long> {
+}
